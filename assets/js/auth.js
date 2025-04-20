9a9540dfdcd33022f8883cff253756cc8a3d48bb
abhinav937 <abhinav.chinnusamy@wisc.edu>
2025-04-20 04:56:13 -0500

    fix4: fixing redirect links for wiki

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -8,10 +8,10 @@ function authenticate() {
         localStorage.setItem("isAuthenticated", "true");
         error.textContent = "";
         // Get redirect URL from query parameter, default to wiki/wiki.html
-        let redirect = "./wempecker/wiki";
+        let redirect = "./wiki/";
         console.log("Initial redirect value:", redirect); // Debug: Log initial redirect
         // Ensure redirect is relative to root
-        if (redirect.startsWith('/wempecker/')) {
+        if (redirect.startsWith('./wiki/')) {
             redirect = redirect.substring(1);
             console.log("Redirect after removing leading slash:", redirect); // Debug: Log modified redirect
         }
